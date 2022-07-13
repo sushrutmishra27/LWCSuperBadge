42d61fc016cadfcfb654103915019837e4f48c09
Sushrut_Sharingan <sushrutmishra27@gmail.com>
2022-07-13 20:07:42 +0530

    changes in JS

--- force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
+++ force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -14,11 +14,8 @@ export default class BoatSearchForm extends LightningElement {
       if (data) {
         this.searchOptions = data.map(type => {
             return {
-
                 label: type.Name,
-      
-                value: type.Id
-      
+                value: type.Id      
               };
         });
         this.searchOptions.unshift({ label: 'All Types', value: '' });
@@ -31,10 +28,16 @@ export default class BoatSearchForm extends LightningElement {
     // Fires event that the search option has changed.
     // passes boatTypeId (value of this.selectedBoatTypeId) in the detail
     handleSearchOptionChange(event) {
+        event.preventDefault();
+        this.selectedBoatTypeId = event.detail.value;
       // Create the const searchEvent
       // searchEvent must be the new custom event search
-      searchEvent;
-      this.dispatchEvent(searchEvent);
+        const searchEvent = new CustomEvent("search", {
+        detail: {
+            boatTypeId: this.selectedBoatTypeId
+        }
+        });
+        this.dispatchEvent(searchEvent);
     }
   }
   
